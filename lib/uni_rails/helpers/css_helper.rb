a510ddc9b0ffe4a6cc7f15de97ef58fb68461559
Alexandre Barret <barret.alx@gmail.com>
2024-03-30 15:03:18 +1300

    Fix mono_rails references

--- lib/uni_rails/helpers/css_helper.rb
+++ lib/uni_rails/helpers/css_helper.rb
@@ -1,7 +1,7 @@
 module UniRails
   module Helpers
     module CSSHelper
-      def mono_rails_css_stylesheet
+      def uni_rails_css_stylesheet
         content_tag(:style) do
           raw App::CSS.css
         end
