de2e5cf49071eed38134961a4ed9e4a29c0842fb
Alexandre Barret <barret.alx@gmail.com>
2024-03-31 22:11:03 +1300

    Create todos api rack test

--- Gemfile
+++ Gemfile
@@ -6,5 +6,8 @@ source "https://rubygems.org"
 gemspec
 
 gem "rake", "~> 13.0"
-
 gem "minitest", "~> 5.0"
+gem "rack-test"
+gem "debug"
+gem "sqlite3"
+gem "puma"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,15 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "uni_rails"
 
 require "minitest/autorun"
+require "rack/test"
+require "debug"
+
+def build_rackup_app(path)
+  app = Rack::Builder.parse_file File.join(Dir.pwd, path)
+  app.configure do
+    config.hosts = ['example.org']
+    config.log_level = :error
+  end
+  app.initialize!
+  app
+end
