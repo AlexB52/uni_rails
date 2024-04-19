da081d5fc0722df1a52e876aba20f2e337d89d20
Alexandre Barret <barret.alx@gmail.com>
2024-04-19 21:19:22 +1200

    Fix rake tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+ENV['SECRET_KEY_BASE'] = 'something'
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "uni_rails"
 
 require "minitest/autorun"
 require "debug"
+
