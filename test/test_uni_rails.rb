309257137d58482951565141bc9bd101ef314bf7
Alexandre Barret <barret.alx@gmail.com>
2026-07-11 19:29:57 +1200

    [BUG] Fix versions of dependencies to prevent API changes

--- test/test_uni_rails.rb
+++ test/test_uni_rails.rb
@@ -6,4 +6,10 @@ class TestUniRails < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::UniRails::VERSION
   end
+
+  def test_rackup_handler_loads_a_server_handler
+    handler = ::UniRails.rackup_handler
+
+    assert handler.respond_to?(:run)
+  end
 end
